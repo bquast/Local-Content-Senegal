ba78ed0cdf4402f129a3e647b5e9b6e17c6d30b2
Bastiaan Quast <bquast@gmail.com>
2015-10-28 09:42:19 +0100

    basic enterprise

--- explore.R
+++ explore.R
@@ -5,8 +5,21 @@
 # laod required libraries
 library(dplyr)
 
-# explore data
+# explore DHS data
 str(SNHR6RFL)
 
-# list the question labels
+# list the DHS question labels
 sapply(SNHR6RFL, attr, "label")
+
+# explore the enterprise data
+str(ent)
+View(ent)
+sapply(ent, attr, "label")
+
+# "Do You Currently Communicate With Clients And Suppliers By E-Mail?"
+ent$c22a
+table(ent$c22a)
+
+# "Establishment has its own website" 
+ent$c22b
+table(ent$c22b)
--- import.R
+++ import.R
@@ -5,5 +5,11 @@
 # load required libraries
 library(haven)
 
-# load STATA files
+# load STATA files from DHS
 SNHR6RFL <- read_dta("data/SNHR6RFL.DTA")
+
+# load STATA files from Enterprise
+ent <- read_dta("Senegal-2014-full data999.dta")
+
+# save enterprise data
+save(file="data/ent.RData", ent)
