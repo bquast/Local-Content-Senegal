7b895b37ffe8fe0dceb6ef9b358e8b4b4f84821d
Bastiaan Quast <bquast@gmail.com>
2015-11-03 11:32:53 +0100

    use new source files

--- import.R
+++ import.R
@@ -6,10 +6,14 @@
 library(haven)
 
 # load STATA files from DHS
-SNHR6RFL <- read_dta("data/SNHR6RFL.DTA")
+hh  <- read_dta("data/SNHR70FL.DTA") # household
+ind <- read_dta("data/SNIR70FL.DTA") # individual
+
+# save DHS data
+save(file = "data/dhs.RData", hh, ind)
 
 # load STATA files from Enterprise
 ent <- read_dta("Senegal-2014-full data999.dta")
 
 # save enterprise data
-save(file="data/ent.RData", ent)
+save(file = "data/ent.RData", ent)
